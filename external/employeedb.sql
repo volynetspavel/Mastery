94cc0f6794a1aca651566f12bc035c17baec61f3
Pavel Volynets <pavelsergeevich11@gmail.com>
2021-05-23 10:52:56 +0300

    create project

---
+++ external/employeedb.sql
@@ -0,0 +1,34 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema employeedb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema employeedb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `employeedb` DEFAULT CHARACTER SET utf8 ;
+USE `employeedb` ;
+
+-- -----------------------------------------------------
+-- Table `employeedb`.`employee`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `employeedb`.`employee` (
+  `employee_id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `department_id` INT NOT NULL,
+  `job_title` VARCHAR(45) NOT NULL,
+  `gender` VARCHAR(45) NOT NULL,
+  `date_of_birth` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`employee_id`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
---
+++ external/fiil_table.sql
@@ -0,0 +1,6 @@
+INSERT INTO employee (first_name, last_name, department_id, job_title, gender, date_of_birth)
+VALUES ("Jonh", "Smith", "1", "developer", "male", "1991-04-12");
+INSERT INTO employee (first_name, last_name, department_id, job_title, gender, date_of_birth)
+VALUES ("Julia", "Verdaski", "2", "teacher", "female", "1993-07-05");
+INSERT INTO employee (first_name, last_name, department_id, job_title, gender, date_of_birth)
+VALUES ("Fabio", "Capollo", "1", "developer", "male", "1992-10-21");
